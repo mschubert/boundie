5cac85dc51166edd2fddd42a44475572747afb20
mschubert <mschu.dev@gmail.com>
2018-06-06 14:29:16 +0200

    add test: compare to NB result

--- tests/testthat/test-resolve_mixture.r
+++ tests/testthat/test-resolve_mixture.r
@@ -1,17 +1,19 @@
 context("resolve mixture")
 
-test_that("100 samples, 2 components", {
+test_that("100 samples, 2 components, same as glm.nb", {
     f1 = runif(100)
     f2 = 1 - f1
     c1 = stats::rnbinom(n=100, size=10, mu=1000)
     c2 = stats::rnbinom(n=100, size=20, mu=10)
     both = round(f1 * c1 + f2 * c2)
 
-    ctl = MASS::glm.nb(both ~ 0 + f1 + f2) # exp(estimate) is if frac->1
-    mod = fit()
-    # sample fractions
-    # sample from c1, c2
-    # add them together in weighted fashion
-})
+    ctl = MASS::glm.nb(both ~ 0 + f1 + f2)
 
-# maybe also do constrained with fake negative component
+    mf = model.frame(both ~ 0 + f1 + f2)
+    terms = attr(mf, "terms")
+    x = model.matrix(terms, mf)
+    y = model.response(mf)
+    mod = fit(x, y)
+
+    expect_true(all(mod$par[1:2] - coef(ctl) < 1e-4))
+})
